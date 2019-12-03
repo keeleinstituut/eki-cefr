c519e0548e9f715e386237fc9a163f67277d5456
Teele <Teele.Pae@twn.ee>
2019-12-03 10:41:27 +0200

    Add new design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public menuItem: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  setItem(selected: string) {
+    this.menuItem = selected;
   }
 
 }
