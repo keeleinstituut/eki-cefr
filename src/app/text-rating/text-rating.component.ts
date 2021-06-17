83c995382b3c136d4d946ab162b4023250ecda19
rabauti <rabauti@gmail.com>
2021-06-17 13:36:01 +0300

    unite textrating grammar and lex views

--- src/app/text-rating/text-rating.component.ts
+++ src/app/text-rating/text-rating.component.ts
@@ -109,13 +109,30 @@ clearText() {
     }
   }
 
-  colors(level): string {
+  colors(index): string {
+
+    var level = this.resultData[index].level;
+    var formsLevel = this.resultDataGrammar[index].level;
+    var level1 = 'color-' + level;
+    var level2 = 'border-'+ formsLevel;
+    if (!level) {
+      level1 = 'color-no';
+    }
+  
+    if (!formsLevel || !(this.form.value.list=='noor')) {
+      level2 = 'border-no';
+    }
+
     for (const color of this.notAllowed) {
       if (color === level) {
-        return 'color-no';
+        level1 = 'color-no';
       }
+      if (color === formsLevel) {
+        level2 = 'border-no';
+      }
+
     }
-    return 'color-' + level;
+    return level1 + ' ' + level2;
   }
 
   phraseColors(wordData): string {
