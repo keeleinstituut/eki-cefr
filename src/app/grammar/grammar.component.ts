e3b3d7a1a29830d6b670e5a83b08e901a4582849
rabauti <rabauti@gmail.com>
2022-09-06 10:26:27 +0300

    Changed project default to 'etLex'

--- src/app/grammar/grammar.component.ts
+++ src/app/grammar/grammar.component.ts
@@ -176,7 +176,7 @@ export class GrammarComponent implements OnInit, OnDestroy {
             types: new FormArray([])
           });
           this.addRow();
-          this.getChildData();
+          this.getAdultData();
         }
       });
     });
